06a0d45c30f471358a25394a4a554aaa0e3dfb50
claudiamartinezv <77359482+claudiamartinezv@users.noreply.github.com>
2023-03-21 22:56:30 -0300

    modificaciones

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 import validator from './validator.js';
 
+//Declarar variable botón 
+const button = document.getElementById("btnValidar");
+
+var arrCreditCard = new Array();
+
+function reverseCard(){
+
+    var creditCardNumber = document.getElementById("tarjetaCredito").value;
+    arrCreditCard[arrCreditCard.length] = creditCardNumber;
+    
+    var reverse = creditCardNumber.reverse();
+    console.log(reverse)
+};
+
+function multiplyCard(reverseCard){
+
+    
+};
+
+
+/*console.log(creditCardNumber);
+
+//Propiedad reverse es el array invertido
+const reverse = creditCardNumber.reverse();
+console.log(reverse);
+
+console.log(creditCardNumber);*/
+
+button.addEventListener("click",()=>{
+
+    validator.isValid()
+});
+
+button.addEventListener("click",()=>{
+
+    validator.maskify()
+});
+
 console.log(validator);
+
+
+
+
+
+
+
+function newFunction() {
+    alert(reverseCard());
+}
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,17 @@
-const validator = {
-  // ...
-};
+const validator ={
+
+  isValid:()=>{
+
+  },
+
+  maskify:()=>{
+    
+  },
+  /*
+  validator.isValid(creditCardNumber): 
+
+  validator.maskify(creditCardNumber): */
+
+}
 
 export default validator;
